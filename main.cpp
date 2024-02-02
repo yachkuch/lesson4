1dfcc8ac4b1b5e1fe6ea6318ddabaefa1342b47c
yachkuch <yachkuch@gmail.com>
2024-02-02 09:55:18 +0300

    12

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include"func.h"
+
+#include <list>
+#include <string>
+#include <vector>
+#include <tuple>
+
 int main(int, char**){
     std::cout << "Hello, from Lesson4!\n";
 }
