2d9801034343ff77a7cbfc571c5d4074294a5066
Nikita Yakush <yachkuch@gmail.com>
2024-02-02 09:44:03 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <cstdint>
 #include"func.h"
+
+#include <list>
+#include <vector>
+#include <string>
+#include <tuple>
+
 int main(int, char**){
     std::cout << "Hello, from Lesson4!\n";
+    std::int8_t a = -1;
+    std::vector<double> vect;
+    int a1 = 1;
+    std::cout<<is_container< int>::value<<std::endl;
+    print_ip(vect);
+    //print_ip(a1)
 }
